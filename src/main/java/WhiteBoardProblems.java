27cfe70b7939afeda52d69b6a855aa12c3fdc5a4
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-11 09:33:53 -0400

    refactor

--- src/main/java/WhiteBoardProblems.java
+++ src/main/java/WhiteBoardProblems.java
@@ -1,12 +1,15 @@
 public class WhiteBoardProblems {
 
-    public int getIndexOfValue(int[] enteredArray, int valueToGet) {
+    public int getIndexOfValue(int[] enteredArray, int valueToGet) { //Linear Search
         return 0;
     }
 
 
-    public int[] getSmallestToLargest(int[] arr) {
+    public int[] getSmallestToLargest(int[] arr) { //Simple Sort
         return new int[0];
 
     }
+
+
+
 }
